5aab023a58182e13fdedeb0292cb0dccc34d90a1
David Pinilla <contact@davidpinilla.com>
2020-10-25 22:31:35 +0100

    add RedSocial smart-contract to interact from the frontend

---
+++ front/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Ethereum_logo_2014.svg" alt="logo" class="logo">
+    <div v-if="account" class="address"><img width=420 height=420 v-bind:src="'data:image/png;base64,' + identicon"> <span>{{account}} </span></div>
+  </div>
+</template>
+<script>
+import Identicon from 'identicon.js'
+export default {
+  props:['account'],
+  created() {
+  },
+  computed : {
+    identicon() {
+        return new Identicon(this.account).toString();
+    }
+},
+}
+</script>
+<style>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.3em;
+  font-size: 1.5em;
+  background-color: rgb(194, 255, 252);
+  box-shadow: 0 0.02em 2em black;
+}
+.address{
+  cursor: pointer;
+  background-color: rgb(91, 255, 241);
+  padding: 0.7em;
+  border-radius: 1.7em;
+  transition: all 1s;
+}
+
+.address:hover{
+
+  background-color: rgb(119, 212, 255);
+}
+img{
+  cursor: pointer;
+  width: 2em;
+  border-radius: 1.7em;
+  height: 2em;
+}
+
+</style>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,44 +1,64 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        front
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <Header v-bind:account="accounts[0]"/>
+
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import Web3 from 'web3';
+import RedSocial from '../contracts/RedSocial.json'
+
+// console.log(web3);
+export default {
+  data:function () {
+    return {
+      accounts:[]
+    }
+  },
+  mounted() {
+    this.loadWeb3();
+    this.loadBlockChainData();
+  },
+  methods: {
+    loadWeb3: async function(){
+      let web3 = new Web3('ws://127.0.0.1:8545');
+      if (process.browser) {
+
+        if(window.ethereum){
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+        }
+        else if(window.web3){
+            window.web3 = new Web3(window.web3.currentProvider)
+        }else{
+            console.warn('No ethereum network found')
+        }
+      }
+    },
+    loadBlockChainData: async function () {
+        const web3 = window.web3;
+        this.accounts = await web3?.eth.getAccounts();
+        this.loadPosts()
+    },
+
+    loadPosts: async function (params) {
+      const networkId = await web3?.eth.net.getId();
+      const contractAddress = RedSocial.networks[networkId]?.address;
+      let RedSocialContract = new web3.eth.Contract(RedSocial.abi,contractAddress);
+      console.log(RedSocialContract);
+    }
+  }
+}
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
   min-height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  flex-direction: column;
 }
 
 .title {
